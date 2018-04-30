318fce3d9fed39d6ebe7f94f53caadb0033e3440
gabriel <jeronimogbariel@gmail.com>
2018-04-30 13:54:37 -0300

    iniciando form login

---
+++ src/app/controllers/HomeControll.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: '../views/home.html',
+  styleUrls: ['../app.component.css']
+})
+export class Home implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+}
