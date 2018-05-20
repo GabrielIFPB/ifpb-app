0c2c55855ff6d447316280ff1506dd0a23869001
gabriel <jeronimogbariel@gmail.com>
2018-05-20 01:08:46 -0300

    fazendo testes com compomentes do material angular 6.0.1

--- src/app/material.app.module.ts
+++ src/app/material.app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-    imports: [ MatToolbarModule, MatButtonModule, ],
-    exports: [ MatToolbarModule, MatButtonModule, ]
+    imports: [ MatToolbarModule, MatButtonModule, MatCardModule, MatSidenavModule, ],
+    exports: [ MatToolbarModule, MatButtonModule, MatCardModule, MatSidenavModule, ]
 })
 export class MaterialAppModule { }
--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AppComponent } from '../app.component';
 import { Home } from '../controllers/HomeControll';
 import { LoginControll } from '../controllers/LoginControll';
 
 const routes: Routes = [
-    { path: 'home',  component: Home },
+    { path: '',  component: Home },
     { path: 'login', component: LoginControll },
 ];
 
