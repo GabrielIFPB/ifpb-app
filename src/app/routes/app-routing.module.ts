d52c6793baa5fcd00fd577592c966c2e3cf8f9f7
gabriel <jeronimogbariel@gmail.com>
2018-05-19 23:13:38 -0300

    toolbar - navegação

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// import { AppComponent } from '../app.component';
 import { Home } from '../controllers/HomeControll';
 import { LoginControll } from '../controllers/LoginControll';
 
 const routes: Routes = [
-    { path: '',      component: Home },
+    { path: 'home',  component: Home },
     { path: 'login', component: LoginControll },
 ];
 
