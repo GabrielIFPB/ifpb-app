e993ffad0da97a289166ac29b9289af6e495e403
gabriel <jeronimogbariel@gmail.com>
2018-05-22 19:15:34 -0300

    adicionando componentes do painel de controle

---
+++ src/app/controllers/PanelControll.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-panel',
+    templateUrl: '../views/panel.html',
+    styleUrls: ['../css/panel.css']
+})
+export class PanelControll implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+}
