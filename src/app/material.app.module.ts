cd15e6c5cd3e8396f724d0d7b4252ddf15a92b0b
gabriel <jeronimogbariel@gmail.com>
2018-05-22 23:05:45 -0300

    fazendo testes com o sidenav do mateial angular

--- src/app/material.app.module.ts
+++ src/app/material.app.module.ts
@@ -3,9 +3,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
-    imports: [ MatToolbarModule, MatButtonModule, MatCardModule, MatSidenavModule, ],
-    exports: [ MatToolbarModule, MatButtonModule, MatCardModule, MatSidenavModule, ]
+    imports: [
+        MatToolbarModule, MatButtonModule, MatCardModule,
+        MatSidenavModule, MatExpansionModule,
+    ],
+    exports: [
+        MatToolbarModule, MatButtonModule, MatCardModule,
+        MatSidenavModule, MatExpansionModule,
+    ]
 })
 export class MaterialAppModule { }
