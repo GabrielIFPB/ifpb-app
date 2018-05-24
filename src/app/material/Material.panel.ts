b638fc425e531e622368eace6af49bd6d38cc745
gabriel <jeronimogbariel@gmail.com>
2018-05-23 23:07:58 -0300

    utilizando materializecss

--- src/app/material/Material.panel.ts
+++ src/app/material/Material.panel.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
-    imports: [ MatSidenavModule, MatListModule ],
-    exports: [ MatSidenavModule, MatListModule ]
+    imports: [ MatSidenavModule, MatListModule, MatGridListModule, ],
+    exports: [ MatSidenavModule, MatListModule, MatGridListModule, ]
 })
 export class MaterialPanelModule { }
