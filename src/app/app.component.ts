5eef153de8a93b68c30cad85638198ad060ff9fa
gabriel <jeronimogbariel@gmail.com>
2018-06-07 14:03:45 -0300

    navbar fixa e sem scroll ok, navbar responsiva e com scroll ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +11,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app fsdfadfadfa';
   login = false;
+  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  // ngOnInit() { }
 }
--- src/app/modules/Panel.module.ts
+++ src/app/modules/Panel.module.ts
@@ -7,15 +7,23 @@ import { PanelControll } from '../controllers/PanelControll';
 import { PanelRoutingModule } from '../routes/panel-routing.module';
 import { MaterialPanelModule } from '../material/Material.panel';
 
+import { DataTableComponent } from '../data-table/data-table.component';
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+
 @NgModule({
 	imports: [
 		CommonModule,
 		FormsModule,
 		PanelRoutingModule,
-		MaterialPanelModule
+		MaterialPanelModule,
+		
+		MatTableModule,
+		MatPaginatorModule,
+		MatSortModule
 	],
 	declarations: [
-		PanelControll
+		PanelControll,
+		DataTableComponent
 	],
 	providers: [
 		
