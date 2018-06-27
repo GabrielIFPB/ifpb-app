97d10da85ed025a919734b1eab1795d088bf758b
gabriel <jeronimogbariel@gmail.com>
2018-06-27 11:01:49 -0300

    melhorias na listagem de alunos com pesquisa e paginação

--- src/app/material/Material.panel.ts
+++ src/app/material/Material.panel.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-    imports: [ MatSidenavModule, MatListModule, MatGridListModule, ],
-    exports: [ MatSidenavModule, MatListModule, MatGridListModule, ]
+    imports: [ MatSidenavModule, MatListModule, MatGridListModule, MatCardModule],
+    exports: [ MatSidenavModule, MatListModule, MatGridListModule, MatCardModule]
 })
 export class MaterialPanelModule { }
--- src/app/modules/Panel.module.ts
+++ src/app/modules/Panel.module.ts
@@ -3,7 +3,6 @@ import { CommonModule }   from '@angular/common';
 import { FormsModule }    from '@angular/forms';
 
 import { PanelControll } from '../controllers/PanelControll';
-// import { LoginService } from '../services/Login.service';
 import { PanelRoutingModule } from '../routes/panel-routing.module';
 import { MaterialPanelModule } from '../material/Material.panel';
 
