1091c641f4af4bd5d362fe0673a058f8693be363
gabriel <jeronimogbariel@gmail.com>
2018-07-24 10:34:03 -0300

    deletando component data-table e criando aluno

--- src/app/modules/Panel.module.ts
+++ src/app/modules/Panel.module.ts
@@ -6,23 +6,15 @@ import { PanelControll } from '../controllers/PanelControll';
 import { PanelRoutingModule } from '../routes/panel-routing.module';
 import { MaterialPanelModule } from '../material/Material.panel';
 
-import { DataTableComponent } from '../data-table/data-table.component';
-import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
-
 @NgModule({
 	imports: [
 		CommonModule,
 		FormsModule,
 		PanelRoutingModule,
 		MaterialPanelModule,
-		
-		MatTableModule,
-		MatPaginatorModule,
-		MatSortModule
 	],
 	declarations: [
 		PanelControll,
-		DataTableComponent
 	],
 	providers: [
 		
