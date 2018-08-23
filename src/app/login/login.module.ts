df0110bfba9239da448c1cfaa15401980a389d83
gabriel <jeronimogbariel@gmail.com>
2018-08-23 19:08:54 -0300

    melhorias em login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     LoginRoutingModule
   ],
-  declarations: []
+  declarations: [ LoginComponent ]
 })
 export class LoginModule { }
