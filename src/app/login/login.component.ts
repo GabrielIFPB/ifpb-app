85b8323a15240bcc14a48522564a8a6a3952fc49
gabriel <jeronimogbariel@gmail.com>
2018-08-23 23:16:01 -0300

    test de submit de informações

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	private _username: string = '';
+	private _password: string = '';
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	submit(): void {
+		if (this._password && this._username) {
+			console.log(this._username);
+			console.log(this._password);
+		} else {
+			console.log('no!');
+		}
+	}
 
 }
