6273dda98e23304a8ef6721c4f3445bf74925e55
gabriel <jeronimogbariel@gmail.com>
2018-09-01 19:20:43 -0300

    melhorias e validadçoes no login

--- src/app/login/login.material.module.ts
+++ src/app/login/login.material.module.ts
@@ -3,9 +3,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-    imports: [ MatInputModule, MatButtonModule, MatCardModule, MatIconModule ],
-    exports: [ MatInputModule, MatButtonModule, MatCardModule, MatIconModule ]
+    imports: [ MatInputModule, MatButtonModule, MatCardModule, MatIconModule, MatSnackBarModule ],
+    exports: [ MatInputModule, MatButtonModule, MatCardModule, MatIconModule, MatSnackBarModule ]
 })
 export class MaterialLoginModule { }
