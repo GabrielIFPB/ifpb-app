88ea70c03c32cdb6cffb54338a0923147a845262
gabriel <jeronimogbariel@gmail.com>
2018-09-01 18:57:03 -0300

    validando form campus

--- src/app/campus/campus.material.modules.ts
+++ src/app/campus/campus.material.modules.ts
@@ -9,15 +9,16 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
 	imports: [ MatTableModule, MatInputModule, MatCardModule,
 				MatIconModule, MatPaginatorModule, MatTooltipModule,
-				MatDialogModule, MatButtonModule, MatRadioModule
+				MatDialogModule, MatButtonModule, MatRadioModule, MatSnackBarModule
 	],
 	exports: [ MatTableModule, MatInputModule, MatCardModule,
 				MatIconModule, MatPaginatorModule, MatTooltipModule,
-				MatDialogModule, MatButtonModule, MatRadioModule
+				MatDialogModule, MatButtonModule, MatRadioModule, MatSnackBarModule
 	]
 })
 export class MaterialCampusModule { }
