c62a14cddf71ef3b2b8971e1962c304acb0087d2
gabriel <jeronimogbariel@gmail.com>
2018-09-08 14:55:08 -0300

    método add alunos pronto

--- src/app/aluno/aluno.service.ts
+++ src/app/aluno/aluno.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Aluno } from './aluno';
+
+const httpOptions = {
+	headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable()
 export class AlunoService {
 
-  constructor() { }
+	private _url = 'http://localhost:3000/alunos';
+
+	private _handleError<T>(operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
+			console.error(error);
+			console.error(error, `Operation: --> ${operation} <--`);
+			console.log(`${operation} failed: ${error.message}`);
+			return of(result as T)
+		}
+	}
+
+	constructor(private _http: HttpClient) { }
+
+	add(aluno: Aluno): Observable<Aluno> {
+		return this._http.post<Aluno>(this._url, aluno, httpOptions)
+			.pipe(
+				tap(() => console.log('add aluno')),
+				catchError(this._handleError<Aluno>('ERROR: ADD CAMPUS'))
+			);
+	}
 }
