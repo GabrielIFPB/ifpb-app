90f7bff9d50f6e2ae69910742c4d48d5824957dd
gabriel <jeronimogbariel@gmail.com>
2018-09-18 21:00:54 -0300

    add module dia refeição

---
+++ src/app/diarefeicao/diarefeicao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-diarefeicao',
+  templateUrl: './diarefeicao.component.html',
+  styleUrls: ['./diarefeicao.component.css']
+})
+export class DiaRefeicaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
