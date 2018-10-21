2dd389e59e6cd81a6225e525991416be17f1019c
gabriel <jeronimogbariel@gmail.com>
2018-10-21 14:54:28 -0300

    fazendo busca por username e password na lista de funcionários

--- src/app/funcionario/funcionario.service.ts
+++ src/app/funcionario/funcionario.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 import { Funcionario } from './funcionario';
 import { Campus } from '../campus/campus';
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,12 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+const httpOptions = {
+	headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class LoginService {
 
-  private _urlFuncio: string = 'http://localhost:3000/funcionarios';
-  private _urlalunos: string = 'http://localhost:3000/alunos';
-  
-  constructor() { }
+	private _urlFuncio: string = 'http://localhost:3000/funcionarios';
+
+	private _handleError<T>(operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
+			console.error(error);
+			console.error(error, `Operation: --> ${operation} <--`);
+			console.log(`${operation} failed: ${error.message}`);
+			return of(result as T)
+		}
+	}
+	
+	constructor(private _http: HttpClient) { }
+
+	logar(username: string, password: string): Observable<any> {
+		if (!username.trim() || !password.trim()) return of([]);
+		let url = `${this._urlFuncio}?email=${username}&password=${password}`;
+		return this._http.get<any>(url, httpOptions)
+			.pipe(
+				tap(() => console.log('Fetched login!')),
+				catchError(this._handleError('error login!', []))
+			);
+	}
 }
